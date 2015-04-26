e91a144a1b9884fc128320a0ecd2900e80e31e23
rmterra <r.mirandaterra@gmail.com>
2015-04-25 23:52:14 -0300

    update load accumulator specification

--- src/NesZord.Core/Microprocessor.cs
+++ src/NesZord.Core/Microprocessor.cs
@@ -7,11 +7,14 @@ namespace NesZord.Core
 {
 	public class Microprocessor
 	{
+		public byte Accumulator { get; private set; }
+
 		public int ProgramCounter { get; private set; }
 
 		public void Start(byte[] program)
 		{
 			this.ProgramCounter = 2;
+			this.Accumulator = program[1];
 		}
 	}
 }
---
+++ src/NesZord.Tests/Describe_microprocessor_accumulator_operations.cs
@@ -0,0 +1,38 @@
+﻿using NesZord.Core;
+using NSpec;
+using Ploeh.AutoFixture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NesZord.Tests
+{
+	public class Describe_microprocessor_accumulator_operations : nspec
+	{
+		private Fixture fixture;
+
+		public void before_each()
+		{
+			this.fixture = new Fixture();
+		}
+
+		public void When_receives_load_the_accumulator_with_immediate_addressing_mode()
+		{
+			Microprocessor processor = null;
+			byte expectedAccumulatorValue = 0x00;
+
+			before = () =>
+			{
+				processor = new Microprocessor();
+				expectedAccumulatorValue = this.fixture.Create<byte>();
+			};
+
+			act = () => { processor.Start(new byte[] { 0xa9, expectedAccumulatorValue }); };
+
+			it["Should add 2 to program counter"] = () => { processor.ProgramCounter.should_be(2); };
+			it["Should set accumulator with received value"] = () => { processor.Accumulator.should_be(expectedAccumulatorValue); };
+		}
+	}
+}
