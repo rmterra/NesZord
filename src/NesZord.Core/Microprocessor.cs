eb067b81e9e100b663378959d0eda1d14328d902
rmterra <r.mirandaterra@gmail.com>
2015-07-11 23:26:14 -0300

    updates processor to organize operations into a dictionary

--- src/NesZord.Core/Microprocessor.cs
+++ src/NesZord.Core/Microprocessor.cs
@@ -12,10 +12,26 @@ namespace NesZord.Core
 
 		private readonly byte[][] memory;
 
+		private readonly Dictionary<OpCode, Action> operations;
+
 		public Microprocessor()
 		{
 			this.memory = new byte[Byte.MaxValue][];
 			for (int i = 0; i < Byte.MaxValue; i++) { this.memory[i] = new byte[Byte.MaxValue]; }
+
+			this.operations = new Dictionary<OpCode, Action>
+			{
+				{ OpCode.ImmediateAddWithCarry, this.AddWithCarry },
+				{ OpCode.AbsoluteStoreAccumulator, this.StoreAccumulator },
+				{ OpCode.AbsoluteStoreXRegister, this.StoreXRegister },
+				{ OpCode.ImmediateLoadXRegister, this.LoadXRegister },
+				{ OpCode.ImmediateLoadAccumulator, this.LoadAccumulator },
+				{ OpCode.TransferFromAccumulatorToX, this.TransferFromAccumulatorToX },
+				{ OpCode.DecrementValueAtX, this.DecrementValueAtX },
+				{ OpCode.BranchIfNotEqual, this.BranchIfNotEqual },
+				{ OpCode.ImmediateCompareXRegister, this.CompareXRegister },
+				{ OpCode.IncrementValueAtX, this.IncrementValueAtX }
+			};
 		}
 
 		public bool Carry { get; private set; }
@@ -35,8 +51,8 @@ namespace NesZord.Core
 
 		public void RunProgram(byte[] program)
 		{
-			LoadProgramToMemory(program);
-			RunProgram();
+			this.LoadProgramToMemory(program);
+			this.ProcessProgramByteWhileNotBreak();
 		}
 
 		private void LoadProgramToMemory(byte[] program)
@@ -53,62 +69,88 @@ namespace NesZord.Core
 			}
 		}
 
-		private void RunProgram()
+		private void ProcessProgramByteWhileNotBreak()
 		{
-			OpCode receivedOpCode = OpCode.Break;
+			OpCode receivedOpCode = (OpCode)this.ReadProgramByte();
+			if (receivedOpCode == OpCode.Break) { return; }
 
-			while ((receivedOpCode = (OpCode)this.ReadProgramByte()) != OpCode.Break)
+			if (this.operations.ContainsKey(receivedOpCode) == false)
 			{
-				if (receivedOpCode == OpCode.ImmediateLoadAccumulator) { this.Accumulator = this.ReadProgramByte(); }
-				else if (receivedOpCode == OpCode.ImmediateLoadXRegister) { this.X = this.ReadProgramByte(); }
-				else if (receivedOpCode == OpCode.TransferFromAccumulatorToX) { this.X = this.Accumulator; }
-				else if (receivedOpCode == OpCode.IncrementValueAtX) { this.X += 1; }
-				else if (receivedOpCode == OpCode.DecrementValueAtX) { this.X -= 1; }
-				else if (receivedOpCode == OpCode.AbsoluteStoreAccumulator)
-				{
-					byte offset = this.ReadProgramByte();
-					byte page = this.ReadProgramByte();
-					this.memory[page][offset] = this.Accumulator;
-				}
-				else if (receivedOpCode == OpCode.AbsoluteStoreXRegister)
-				{
-					byte offset = this.ReadProgramByte();
-					byte page = this.ReadProgramByte();
-					this.memory[page][offset] = this.X;
-				}
-				else if (receivedOpCode == OpCode.ImmediateAddWithCarry)
-				{
-					int result = this.Accumulator + this.ReadProgramByte();
-					this.Accumulator = (byte)(result & 0xff);
-					this.Carry = (result >> 8) > 0;
-				}
-				else if (receivedOpCode == OpCode.ImmediateCompareXRegister)
-				{
-					int result = this.X - this.ReadProgramByte();
-					this.Carry = result >= 0;
-					this.Zero = result == 0;
-				}
-				else if (receivedOpCode == OpCode.BranchIfNotEqual)
-				{
-					if (this.Zero)
-					{
-						this.ReadProgramByte();
-						continue;
-					}
-
-					byte memoryPage = (byte)(this.ProgramCounter >> 8);
-					byte memoryOffset = (byte)(this.ProgramCounter & 0xff);
-					byte branchOffset = this.ValueAt(memoryPage, memoryOffset);
-
-					int offset = 0xff ^ branchOffset;
-					this.ProgramCounter -= offset;
-				}
-				else
-				{
-					String error = String.Format(CultureInfo.InvariantCulture, "unknown opcode {0}", receivedOpCode);
-					throw new InvalidOperationException(error);
-				}
+				String error = String.Format(CultureInfo.InvariantCulture, "unknown opcode {0}", receivedOpCode);
+				throw new InvalidOperationException(error);
 			}
+
+			this.operations[receivedOpCode]();
+			this.ProcessProgramByteWhileNotBreak();
+		}
+
+		private void AddWithCarry()
+		{
+			int result = this.Accumulator + this.ReadProgramByte();
+			this.Accumulator = (byte)(result & 0xff);
+			this.Carry = (result >> 8) > 0;
+		}
+
+		private void StoreAccumulator()
+		{
+			byte offset = this.ReadProgramByte();
+			byte page = this.ReadProgramByte();
+			this.memory[page][offset] = this.Accumulator;
+		}
+
+		private void StoreXRegister()
+		{
+			byte offset = this.ReadProgramByte();
+			byte page = this.ReadProgramByte();
+			this.memory[page][offset] = this.X;
+		}
+
+		private void LoadXRegister()
+		{
+			this.X = this.ReadProgramByte();
+		}
+
+		private void LoadAccumulator()
+		{
+			this.Accumulator = this.ReadProgramByte();
+		}
+
+		private void TransferFromAccumulatorToX()
+		{
+			this.X = this.Accumulator;
+		}
+
+		private void DecrementValueAtX()
+		{
+			this.X -= 1;
+		}
+
+		private void BranchIfNotEqual()
+		{
+			if (this.Zero)
+			{
+				this.ReadProgramByte();
+				return;
+			}
+
+			byte memoryPage = (byte)(this.ProgramCounter >> 8);
+			byte memoryOffset = (byte)(this.ProgramCounter & 0xff);
+			byte branchOffset = this.ValueAt(memoryPage, memoryOffset);
+
+			int offset = 0xff ^ branchOffset;
+			this.ProgramCounter -= offset;
+		}
+
+		private void CompareXRegister()
+		{
+			int result = this.X - this.ReadProgramByte();
+			this.Carry = result >= 0;
+			this.Zero = result == 0;
+		}
+
+		private void IncrementValueAtX()
+		{
+			this.X += 1;
 		}
 
 		private byte ReadProgramByte()
