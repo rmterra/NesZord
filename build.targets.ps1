69ffab74070a96dba43e132b711d9294e67edd7f
rmterra <r.mirandaterra@gmail.com>
2015-07-19 11:24:55 -0300

    updates build task to use appveyor logger and post specs

--- build.targets.ps1
+++ build.targets.ps1
@@ -1,10 +1,15 @@
 Properties {
 	$root_dir = Split-Path $psake.build_script_file
 	$artifacts_dir = "$root_dir\bin"
-	$testdll_dir = "$artifacts_dir\NesZord.Tests.dll"
 	$source_dir = "$root_dir\src"
+	$log_dir = "$root_dir\log"
+	
+	$testdll_dir = "$artifacts_dir\NesZord.Tests.dll"
+	
 	$solution_dir = "$source_dir\NesZord.sln"
 	$nspecrunner_dir = "$source_dir\packages\nspec.0.9.68\tools\nspecrunner.exe"
+	
+	$isAppVeyor = $env:APPVEYOR -eq $true
 }
 
 FormatTaskName (("-"*25) + "[{0}]" + ("-"*25))
@@ -12,13 +17,8 @@ FormatTaskName (("-"*25) + "[{0}]" + ("-"*25))
 Task Default -Depends Setup, Clean, Build, Test
 
 Task Setup {
-	Write-Host "Creating build artifacts directory" -ForegroundColor Green
-	if (Test-Path $artifacts_dir) 
-	{	
-		rd $artifacts_dir -rec -force | out-null
-	}
-	
-	mkdir $artifacts_dir | out-null
+	CreateCleanDir "build artifacts" $artifacts_dir
+	CreateCleanDir "logs" $log_dir
 }
 
 Task Clean {
@@ -28,10 +28,27 @@ Task Clean {
 
 Task Build -Depends Clean {	
 	Write-Host "Building NesZord.sln" -ForegroundColor Green
-	Exec { msbuild $solution_dir /t:Build /p:Configuration=Debug /v:quiet /p:OutDir=$artifacts_dir } 
+	if($isAppVeyor) {
+		Exec { msbuild $solution_dir /t:Build /p:Configuration=Debug /v:quiet /p:OutDir=$artifacts_dir /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" } 
+	}
+	else {
+		Exec { msbuild $solution_dir /t:Build /p:Configuration=Debug /v:quiet /p:OutDir=$artifacts_dir } 
+	}
 }
 
 Task Test {	
 	Write-Host "Testing NesZord.sln" -ForegroundColor Green
 	Exec { & $nspecrunner_dir $testdll_dir } 
+	Exec { & $nspecrunner_dir $testdll_dir --formatter=XUnitFormatter > "$log_dir\xunit-results.xml" } 
+	
+	if($isAppVeyor) {
+		$wc = New-Object 'System.Net.WebClient'
+		$wc.UploadFile("https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path "$log_dir\xunit-results.xml"))
+	}
+}
+
+function CreateCleanDir($dirName, $dirPath) {
+	Write-Host "Creating $dirName directory" -ForegroundColor Green
+	if (Test-Path $dirPath) { rd $dirPath -rec -force | out-null }
+	mkdir $dirPath | out-null
 }
