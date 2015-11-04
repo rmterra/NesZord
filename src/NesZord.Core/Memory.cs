d9bbed8129b30b04a6c6f0a487f09a9f71a59083
rmterra <r.mirandaterra@gmail.com>
2015-11-04 16:15:49 -0200

    adds memory class and specs

---
+++ src/NesZord.Core/Memory.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NesZord.Core
+{
+	public class Memory
+	{
+		public const byte ZERO_PAGE = 0x00;
+
+		public const int PROGRAM_ROM_START = 0x0600;
+
+		private readonly byte[][] memory;
+
+		public Memory()
+		{
+			this.memory = new byte[Byte.MaxValue][];
+			for (int i = 0; i < Byte.MaxValue; i++) { this.memory[i] = new byte[Byte.MaxValue]; }
+		}
+
+		public void LoadMemory(byte[] program)
+		{
+			byte page = PROGRAM_ROM_START >> 8;
+			byte offset = PROGRAM_ROM_START & 0xff;
+
+			for (int i = 0; i < program.Length; i++)
+			{
+				this.memory[page][offset] = program[i];
+				offset++;
+			}
+		}
+
+		public void WriteZeroPage(byte offset, byte value)
+		{
+			this.Write(new MemoryLocation(offset, 0x00), value);
+		}
+
+		public void Write(byte page, byte offset, byte value)
+		{
+			this.Write(new MemoryLocation(offset, page), value);
+		}
+
+		public void Write(MemoryLocation location, byte value)
+		{
+			this.memory[location.Page][location.Offset] = value;
+		}
+
+		public byte Read(byte page, byte offset)
+		{
+			return this.Read(new MemoryLocation(offset, page));
+		}
+
+		public byte Read(MemoryLocation location)
+		{
+			return this.memory[location.Page][location.Offset];
+		}
+	}
+}
---
+++ src/NesZord.Tests/Describe_memory.cs
@@ -0,0 +1,34 @@
+﻿using NesZord.Core;
+using NSpec;
+using Ploeh.AutoFixture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NesZord.Tests
+{
+	public class Describe_memory : nspec
+	{
+		private readonly Fixture fixture = new Fixture();
+
+		private Memory memory;
+
+		public void before_each() { this.memory = new Memory(); }
+
+		public void When_load_memory_with_program()
+		{
+			var randomByteCount = new Random().Next(0, 10);
+			var bytes = this.fixture.CreateMany<byte>(randomByteCount);
+
+			act = () => { this.memory.LoadMemory(bytes.ToArray()); };
+
+			for (int i = 0; i < randomByteCount - 1; i++)
+			{
+				String spec = String.Format("should memory at at 0x06 0x0{0:x} be 0x{1:x}", i, bytes.ElementAt(i));
+				it[spec] = () => { this.memory.Read(0x06, (byte)i).should_be(bytes.ElementAt(i)); };
+			}
+		}
+	}
+}
