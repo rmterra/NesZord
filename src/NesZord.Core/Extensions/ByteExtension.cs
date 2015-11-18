d57fd401d850c197201a8576a0de7a43f3a214ce
rmterra <r.mirandaterra@gmail.com>
2015-11-18 14:47:33 -0200

    remove magic numbers

--- src/NesZord.Core/Extensions/ByteExtension.cs
+++ src/NesZord.Core/Extensions/ByteExtension.cs
@@ -4,6 +4,10 @@ namespace NesZord.Core.Extensions
 {
 	public static class ByteExtension
 	{
+		public const int HALF_BYTE = 4;
+
+		public const int BCD_MAXIMUM_VALUE = 9;
+
 		public static bool GetBitAt(this byte value, int index)
 		{
 			return (value & (1 << index)) != 0;
@@ -11,13 +15,14 @@ namespace NesZord.Core.Extensions
 
 		public static byte ConvertToBcd(this byte value)
 		{
-			var tenColumn = value >> 4;
-			if (tenColumn > 9) { throw new InvalidOperationException("Ten column overflow BCD maximum value"); }
+			var tenColumn = value >> HALF_BYTE;
+			if (tenColumn > BCD_MAXIMUM_VALUE) { throw new InvalidOperationException("Ten column overflow BCD maximum value"); }
 
-			var unitColumn = (tenColumn << 4) ^ value;
-			if (unitColumn > 9) { throw new InvalidOperationException("Unit column overflow BCD maximum value"); }
+			var unitColumn = (tenColumn << HALF_BYTE) ^ value;
+			if (unitColumn > BCD_MAXIMUM_VALUE) { throw new InvalidOperationException("Unit column overflow BCD maximum value"); }
 
-			return (byte)((tenColumn * 10) + unitColumn);
-        }
-    }
+			tenColumn *= 10;
+			return (byte)(tenColumn + unitColumn);
+		}
+	}
 }
