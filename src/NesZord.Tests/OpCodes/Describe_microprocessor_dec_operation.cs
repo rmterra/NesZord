117dfee5b42f41884387c8ff3ade2e4d1e650171
rmterra <r.mirandaterra@gmail.com>
2016-06-05 22:47:12 -0300

    Refactor dec operation spec to remove duplicated code

--- src/NesZord.Tests/OpCodes/Describe_microprocessor_dec_operation.cs
+++ src/NesZord.Tests/OpCodes/Describe_microprocessor_dec_operation.cs
@@ -21,177 +21,71 @@ namespace NesZord.Tests.OpCodes
 
 		public void When_use_zero_page_addressing_mode()
 		{
-			var expectedValue = default(byte);
 			var randomOffset = default(byte);
 
-			before = () => 
-			{
-				expectedValue = fixture.Create<byte>();
-				randomOffset = fixture.Create<byte>();
+			before = () => randomOffset = fixture.Create<byte>();
 
-				this.memory.WriteZeroPage(randomOffset, (byte)(expectedValue + 1));
-			};
+			act = () => processor.RunProgram(new byte[] { (byte)OpCode.DEC_ZeroPage, randomOffset });
 
-			act = () =>
-			{
-				processor.RunProgram(new byte[] { (byte)OpCode.DEC_ZeroPage, randomOffset });
-			};
-
-			it["should decrement 1 to memory value"] = () => 
-			{
-				this.memory.Read(randomOffset, Memory.ZERO_PAGE).should_be(expectedValue);
-			};
-
-			context["given that memory value is 0x01"] = () => 
-			{
-				before = () =>
-				{
-					expectedValue = 0x00;
-					this.memory.WriteZeroPage(randomOffset, (byte)(expectedValue + 1));
-				};
-
-				it["should set zero flag"] = () => { processor.Zero.should_be_true(); };
-
-				it["should not set negative flag"] = () => { processor.Negative.should_be_false(); };
-			};
-
-			context["given that final memory value sign bit is 1"] = () =>
-			{
-				before = () =>
-				{
-					expectedValue = (byte)new Random().Next(0x80, 0xff);
-					this.memory.WriteZeroPage(randomOffset, (byte)(expectedValue + 1));
-				};
-
-				it["should not set zero flag"] = () => { processor.Zero.should_be_false(); };
-
-				it["should set negative flag"] = () => { processor.Negative.should_be_true(); };
-			};
+			this.DefineSpecs(
+				(b) => this.memory.WriteZeroPage(randomOffset, b),
+				() => this.memory.Read(randomOffset, Memory.ZERO_PAGE));
 		}
 
 		public void When_use_zero_page_x_addressing_mode()
 		{
-			var expectedValue = default(byte);
 			var randomOffset = default(byte);
 			var xRegisterValue = default(byte);
 
 			before = () =>
 			{
-				expectedValue = fixture.Create<byte>();
 				randomOffset = fixture.Create<byte>();
 				xRegisterValue = fixture.Create<byte>();
-
-				this.memory.WriteZeroPage((byte)(xRegisterValue + randomOffset), (byte)(expectedValue + 1));
 			};
 
 			act = () =>
 			{
-				processor.RunProgram(new byte[] 
+				processor.RunProgram(new byte[]
 				{
 					(byte)OpCode.LDX_Immediate, xRegisterValue,
 					(byte)OpCode.DEC_ZeroPageX, randomOffset
 				});
 			};
 
-			it["should decrement 1 to memory value"] = () =>
-			{
-				this.memory.Read((byte)(xRegisterValue + randomOffset), Memory.ZERO_PAGE).should_be(expectedValue);
-			};
-
-			context["given that memory value is 0x01"] = () =>
-			{
-				before = () =>
-				{
-					expectedValue = 0x00;
-					this.memory.WriteZeroPage((byte)(xRegisterValue + randomOffset), (byte)(expectedValue + 1));
-				};
-
-				it["should set zero flag"] = () => { processor.Zero.should_be_true(); };
-
-				it["should not set negative flag"] = () => { processor.Negative.should_be_false(); };
-			};
-
-			context["given that final memory value sign bit is 1"] = () =>
-			{
-				before = () =>
-				{
-					expectedValue = (byte)new Random().Next(0x80, 0xff);
-					this.memory.WriteZeroPage((byte)(xRegisterValue + randomOffset), (byte)(expectedValue + 1));
-				};
-
-				it["should not set zero flag"] = () => { processor.Zero.should_be_false(); };
-
-				it["should set negative flag"] = () => { processor.Negative.should_be_true(); };
-			};
+			this.DefineSpecs(
+				(b) => this.memory.WriteZeroPage((byte)(xRegisterValue + randomOffset), b),
+				() => this.memory.Read((byte)(xRegisterValue + randomOffset), Memory.ZERO_PAGE));
 		}
 
 		public void When_use_absolute_addressing_mode()
 		{
-			var expectedValue = default(byte);
 			var randomOffset = default(byte);
 			var randomPage = default(byte);
 
 			before = () =>
 			{
-				expectedValue = fixture.Create<byte>();
 				randomOffset = fixture.Create<byte>();
 				randomPage = fixture.Create<byte>();
-
-				this.memory.Write(randomOffset, randomPage, (byte)(expectedValue + 1));
 			};
 
-			act = () =>
-			{
-				processor.RunProgram(new byte[] { (byte)OpCode.DEC_Absolute, randomOffset, randomPage });
-			};
+			act = () => processor.RunProgram(new byte[] { (byte)OpCode.DEC_Absolute, randomOffset, randomPage });
 
-			it["should decrement 1 to memory value"] = () =>
-			{
-				this.memory.Read(randomOffset, randomPage).should_be(expectedValue);
-			};
-
-			context["given that memory value is 0x01"] = () =>
-			{
-				before = () =>
-				{
-					expectedValue = 0x00;
-					this.memory.Write(randomOffset, randomPage, (byte)(expectedValue + 1));
-				};
-
-				it["should set zero flag"] = () => { processor.Zero.should_be_true(); };
-
-				it["should not set negative flag"] = () => { processor.Negative.should_be_false(); };
-			};
-
-			context["given that final memory value sign bit is 1"] = () =>
-			{
-				before = () =>
-				{
-					expectedValue = (byte)new Random().Next(0x80, 0xff);
-					this.memory.Write(randomOffset, randomPage, (byte)(expectedValue + 1));
-				};
-
-				it["should not set zero flag"] = () => { processor.Zero.should_be_false(); };
-
-				it["should set negative flag"] = () => { processor.Negative.should_be_true(); };
-			};
+			this.DefineSpecs(
+				(b) => this.memory.Write(randomOffset, randomPage, b),
+				() => this.memory.Read(randomOffset, randomPage));
 		}
 
 		public void When_use_absolute_x_addressing_mode()
 		{
-			var expectedValue = default(byte);
 			var randomOffset = default(byte);
 			var randomPage = default(byte);
 			var xRegisterValue = default(byte);
 
 			before = () =>
 			{
-				expectedValue = fixture.Create<byte>();
 				randomOffset = fixture.Create<byte>();
 				randomPage = fixture.Create<byte>();
 				xRegisterValue = fixture.Create<byte>();
-
-				this.memory.Write((byte)(xRegisterValue + randomOffset), randomPage, (byte)(expectedValue + 1));
 			};
 
 			act = () =>
@@ -203,34 +97,30 @@ namespace NesZord.Tests.OpCodes
 				});
 			};
 
-			it["should decrement 1 to memory value"] = () =>
-			{
-				this.memory.Read((byte)(xRegisterValue + randomOffset), randomPage).should_be(expectedValue);
-			};
+			this.DefineSpecs(
+				(b) => this.memory.Write((byte)(xRegisterValue + randomOffset), randomPage, b),
+				() => this.memory.Read((byte)(xRegisterValue + randomOffset), randomPage));
+		}
+
+		private void DefineSpecs(Action<byte> setByteToDecrement, Func<byte> getDecrementedByte)
+		{
+			before = () => setByteToDecrement(0x05);
+
+			it["should decrement 1 to memory value"] = () => getDecrementedByte().should_be(0x04);
 
 			context["given that memory value is 0x01"] = () =>
 			{
-				before = () =>
-				{
-					expectedValue = 0x00;
-					this.memory.Write((byte)(xRegisterValue + randomOffset), randomPage, (byte)(expectedValue + 1));
-				};
+				before = () => setByteToDecrement(0x01);
 
 				it["should set zero flag"] = () => { processor.Zero.should_be_true(); };
-
 				it["should not set negative flag"] = () => { processor.Negative.should_be_false(); };
 			};
 
-			context["given that final memory value sign bit is 1"] = () =>
+			context["given that sign bit of memory value is set"] = () =>
 			{
-				before = () =>
-				{
-					expectedValue = (byte)new Random().Next(0x80, 0xff);
-					this.memory.Write((byte)(xRegisterValue + randomOffset), randomPage, (byte)(expectedValue + 1));
-				};
+				before = () => setByteToDecrement(0x81);
 
 				it["should not set zero flag"] = () => { processor.Zero.should_be_false(); };
-
 				it["should set negative flag"] = () => { processor.Negative.should_be_true(); };
 			};
 		}
