6bb8bd89a3e989ff962c2efafac490658777e016
rmterra <r.mirandaterra@gmail.com>
2016-08-02 08:54:36 -0300

    Refactor eor operation spec to remove duplicated code

--- src/NesZord.Tests/OpCodes/Describe_microprocessor_eor_operation.cs
+++ src/NesZord.Tests/OpCodes/Describe_microprocessor_eor_operation.cs
@@ -2,339 +2,228 @@
 using NSpec;
 using Ploeh.AutoFixture;
 using NesZord.Core.Extensions;
+using System;
 
 namespace NesZord.Tests.OpCodes
 {
-	public class Describe_microprocessor_eor_operation : nspec
+	public class Describe_microprocessor_eor_operation : Describe_microprocessor_operation
 	{
-		private static readonly Fixture fixture = new Fixture();
-
-		private MemoryMock memory;
-
-		private Microprocessor processor;
-
-		public void before_each()
-		{
-			this.memory = new MemoryMock();
-			this.processor = new Microprocessor(this.memory);
-		}
-
 		public void When_use_immediate_addressing_mode()
 		{
+			var accumulatorValue = default(byte);
 			var byteToCompare = default(byte);
 
-			before = () => { byteToCompare = fixture.Create<byte>(); };
-
-			act = () =>
+			this.RunProgram(() => new byte[]
 			{
-				this.processor.RunProgram(new byte[]
-				{
-					(byte) OpCode.LDA_Absolute, fixture.Create<byte>(),
-					(byte) OpCode.TAX_Implied,
-					(byte) OpCode.EOR_Immediate, byteToCompare
-				});
-			};
+				(byte) OpCode.LDA_Immediate, accumulatorValue,
+				(byte) OpCode.EOR_Immediate, byteToCompare
+			});
 
-			it["should set bitwise exclusive 'or' result on accumulator"] = () =>
-			{
-				this.processor.Accumulator.Value.should_be(this.processor.X.ExlusiveOr(byteToCompare));
-			};
-
-			it["should set negative flag equal to seventh accumulator bit"] = () =>
-			{
-				this.processor.Negative.should_be(this.processor.Accumulator.IsSignBitSet);
-			};
-
-			it["should set zero flag when accumulator is 0"] = () =>
-			{
-				this.processor.Zero.should_be(this.processor.Accumulator.IsValueEqualZero);
-			};
+			this.DefineSpecs(
+				(b) => accumulatorValue = b,
+				(b) => byteToCompare = b);
 		}
 
 		public void When_use_zero_page_addressing_mode()
 		{
+			var accumulatorValue = default(byte);
 			var randomOffset = default(byte);
-			var byteToCompare = default(byte);
 
-			before = () => 
-			{
-				randomOffset = fixture.Create<byte>();
-				byteToCompare = fixture.Create<byte>();
-				this.memory.WriteZeroPage(randomOffset, byteToCompare);
-			};
+			before = () => randomOffset = this.Fixture.Create<byte>();
 
-			act = () =>
+			this.RunProgram(() => new byte[]
 			{
-				this.processor.RunProgram(new byte[]
-				{
-					(byte) OpCode.LDA_Absolute, fixture.Create<byte>(),
-					(byte) OpCode.TAX_Implied,
-					(byte) OpCode.EOR_ZeroPage, randomOffset
-				});
-			};
+				(byte) OpCode.LDA_Immediate, accumulatorValue,
+				(byte) OpCode.EOR_ZeroPage, randomOffset
+			});
 
-			it["should set bitwise exclusive 'or' result on accumulator"] = () =>
-			{
-				this.processor.Accumulator.Value.should_be(this.processor.X.ExlusiveOr(byteToCompare));
-			};
-
-			it["should set negative flag equal to seventh accumulator bit"] = () =>
-			{
-				this.processor.Negative.should_be(this.processor.Accumulator.IsSignBitSet);
-			};
-
-			it["should set zero flag when accumulator is 0"] = () =>
-			{
-				this.processor.Zero.should_be(this.processor.Accumulator.IsValueEqualZero);
-			};
+			this.DefineSpecs(
+				(b) => accumulatorValue = b, 
+				(b) => this.Memory.WriteZeroPage(randomOffset, b));
 		}
 
 		public void When_use_zero_page_x_addressing_mode()
 		{
+			var accumulatorValue = default(byte);
 			var randomOffset = default(byte);
-			var byteToCompare = default(byte);
 			var xRegisterValue = default(byte);
 
 			before = () =>
 			{
-				randomOffset = fixture.Create<byte>();
-				byteToCompare = fixture.Create<byte>();
-				xRegisterValue = fixture.Create<byte>();
-				this.memory.WriteZeroPage((byte)(xRegisterValue + randomOffset), byteToCompare);
+				randomOffset = this.Fixture.Create<byte>();
+				xRegisterValue = this.Fixture.Create<byte>();
 			};
 
-			act = () =>
+			this.RunProgram(() => new byte[]
 			{
-				this.processor.RunProgram(new byte[]
-				{
-					(byte) OpCode.LDA_Absolute, fixture.Create<byte>(),
-					(byte) OpCode.TAY_Implied,
-					(byte) OpCode.LDX_Immediate, xRegisterValue,
-					(byte) OpCode.EOR_ZeroPageX, randomOffset
-				});
-			};
+				(byte) OpCode.LDA_Immediate, accumulatorValue,
+				(byte) OpCode.LDX_Immediate, xRegisterValue,
+				(byte) OpCode.EOR_ZeroPageX, randomOffset
+			});
 
-			it["should set bitwise exclusive 'or' result on accumulator"] = () =>
-			{
-				this.processor.Accumulator.Value.should_be(this.processor.Y.Value ^ byteToCompare);
-			};
-
-			it["should set negative flag equal to seventh accumulator bit"] = () =>
-			{
-				this.processor.Negative.should_be(this.processor.Accumulator.IsSignBitSet);
-			};
-
-			it["should set zero flag when accumulator is 0"] = () =>
-			{
-				this.processor.Zero.should_be(this.processor.Accumulator.IsValueEqualZero);
-			};
+			this.DefineSpecs(
+				(b) => accumulatorValue = b,
+				(b) => this.Memory.WriteZeroPage((byte)(xRegisterValue + randomOffset), b));
 		}
 
 		public void When_use_absolute_addressing_mode()
 		{
+			var accumulatorValue = default(byte);
 			var randomOffset = default(byte);
 			var randomPage = default(byte);
-			var byteToCompare = default(byte);
 
 			before = () =>
 			{
-				randomOffset = fixture.Create<byte>();
-				byteToCompare = fixture.Create<byte>();
-				randomPage = fixture.Create<byte>();
-				this.memory.Write(randomOffset, randomPage, byteToCompare);
+				randomOffset = this.Fixture.Create<byte>();
+				randomPage = this.Fixture.Create<byte>();
 			};
 
-			act = () =>
+			this.RunProgram(() => new byte[]
 			{
-				this.processor.RunProgram(new byte[]
-				{
-					(byte) OpCode.LDA_Absolute, fixture.Create<byte>(),
-					(byte) OpCode.TAX_Implied,
-					(byte) OpCode.EOR_Absolute, randomOffset, randomPage
-				});
-			};
+				(byte) OpCode.LDA_Immediate, accumulatorValue,
+				(byte) OpCode.EOR_Absolute, randomOffset, randomPage
+			});
 
-			it["should set bitwise exclusive 'or' result on accumulator"] = () =>
-			{
-				this.processor.Accumulator.Value.should_be(this.processor.X.ExlusiveOr(byteToCompare));
-			};
-
-			it["should set negative flag equal to seventh accumulator bit"] = () =>
-			{
-				this.processor.Negative.should_be(this.processor.Accumulator.IsSignBitSet);
-			};
-
-			it["should set zero flag when accumulator is 0"] = () =>
-			{
-				this.processor.Zero.should_be(this.processor.Accumulator.IsValueEqualZero);
-			};
+			this.DefineSpecs(
+				(b) => accumulatorValue = b,
+				(b) => this.Memory.Write(randomOffset, randomPage, b));
 		}
 
 		public void When_use_absolute_x_addressing_mode()
 		{
+			var accumulatorValue = default(byte);
 			var randomOffset = default(byte);
 			var randomPage = default(byte);
-			var byteToCompare = default(byte);
 			var xRegisterValue = default(byte);
 
 			before = () =>
 			{
-				randomOffset = fixture.Create<byte>();
-				byteToCompare = fixture.Create<byte>();
-				randomPage = fixture.Create<byte>();
-				xRegisterValue = fixture.Create<byte>();
-				this.memory.Write((byte)(xRegisterValue + randomOffset), randomPage, byteToCompare);
+				randomOffset = this.Fixture.Create<byte>();
+				randomPage = this.Fixture.Create<byte>();
+				xRegisterValue = this.Fixture.Create<byte>();
 			};
 
-			act = () =>
+			this.RunProgram(() => new byte[]
 			{
-				this.processor.RunProgram(new byte[]
-				{
-					(byte) OpCode.LDA_Absolute, fixture.Create<byte>(),
-					(byte) OpCode.TAY_Implied,
-					(byte) OpCode.LDX_Immediate, xRegisterValue,
-					(byte) OpCode.EOR_AbsoluteX, randomOffset, randomPage
-				});
-			};
+				(byte) OpCode.LDA_Immediate, accumulatorValue,
+				(byte) OpCode.LDX_Immediate, xRegisterValue,
+				(byte) OpCode.EOR_AbsoluteX, randomOffset, randomPage
+			});
 
-			it["should set bitwise exclusive 'or' result on accumulator"] = () =>
-			{
-				this.processor.Accumulator.Value.should_be(this.processor.Y.Value ^ byteToCompare);
-			};
-
-			it["should set negative flag equal to seventh accumulator bit"] = () =>
-			{
-				this.processor.Negative.should_be(this.processor.Accumulator.IsSignBitSet);
-			};
-
-			it["should set zero flag when accumulator is 0"] = () =>
-			{
-				this.processor.Zero.should_be(this.processor.Accumulator.IsValueEqualZero);
-			};
+			this.DefineSpecs(
+				(b) => accumulatorValue = b,
+				(b) => this.Memory.Write((byte)(xRegisterValue + randomOffset), randomPage, b));
 		}
 
 		public void When_use_absolute_y_addressing_mode()
 		{
+			var accumulatorValue = default(byte);
 			var randomOffset = default(byte);
 			var randomPage = default(byte);
-			var byteToCompare = default(byte);
 			var yRegisterValue = default(byte);
 
 			before = () =>
 			{
-				randomOffset = fixture.Create<byte>();
-				byteToCompare = fixture.Create<byte>();
-				randomPage = fixture.Create<byte>();
-				yRegisterValue = fixture.Create<byte>();
-				this.memory.Write((byte)(yRegisterValue + randomOffset), randomPage, byteToCompare);
+				randomOffset = this.Fixture.Create<byte>();
+				randomPage = this.Fixture.Create<byte>();
+				yRegisterValue = this.Fixture.Create<byte>();
 			};
 
-			act = () =>
+			this.RunProgram(() => new byte[]
 			{
-				this.processor.RunProgram(new byte[]
-				{
-					(byte) OpCode.LDA_Absolute, fixture.Create<byte>(),
-					(byte) OpCode.TAX_Implied,
-					(byte) OpCode.LDY_Immediate, yRegisterValue,
-					(byte) OpCode.EOR_AbsoluteY, randomOffset, randomPage
-				});
-			};
+				(byte) OpCode.LDA_Immediate, accumulatorValue,
+				(byte) OpCode.LDY_Immediate, yRegisterValue,
+				(byte) OpCode.EOR_AbsoluteY, randomOffset, randomPage
+			});
 
-			it["should set bitwise exclusive 'or' result on accumulator"] = () =>
-			{
-				this.processor.Accumulator.Value.should_be(this.processor.X.ExlusiveOr(byteToCompare));
-			};
-
-			it["should set negative flag equal to seventh accumulator bit"] = () =>
-			{
-				this.processor.Negative.should_be(this.processor.Accumulator.IsSignBitSet);
-			};
-
-			it["should set zero flag when accumulator is 0"] = () =>
-			{
-				this.processor.Zero.should_be(this.processor.Accumulator.IsValueEqualZero);
-			};
+			this.DefineSpecs(
+				(b) => accumulatorValue = b,
+				(b) => this.Memory.Write((byte)(yRegisterValue + randomOffset), randomPage, b));
 		}
 
 		public void When_use_indexed_indirect_addressing_mode()
 		{
+			var accumulatorValue = default(byte);
 			var randomOffset = default(byte);
-			var byteToCompare = default(byte);
 			var xRegisterValue = default(byte);
 
 			before = () =>
 			{
-				randomOffset = fixture.Create<byte>();
-				byteToCompare = fixture.Create<byte>();
-				xRegisterValue = fixture.Create<byte>();
-				this.memory.MockIndexedIndirectMemoryWrite(randomOffset, xRegisterValue, byteToCompare);
+				randomOffset = this.Fixture.Create<byte>();
+				xRegisterValue = this.Fixture.Create<byte>();
 			};
 
-			act = () =>
+			this.RunProgram(() => new byte[]
 			{
-				this.processor.RunProgram(new byte[]
-				{
-					(byte) OpCode.LDA_Absolute, fixture.Create<byte>(),
-					(byte) OpCode.TAY_Implied,
-					(byte) OpCode.LDX_Immediate, xRegisterValue,
-					(byte) OpCode.EOR_IndexedIndirect, randomOffset
-				});
-			};
+				(byte) OpCode.LDA_Immediate, accumulatorValue,
+				(byte) OpCode.LDX_Immediate, xRegisterValue,
+				(byte) OpCode.EOR_IndexedIndirect, randomOffset
+			});
 
-			it["should set bitwise exclusive 'or' result on accumulator"] = () =>
-			{
-				this.processor.Accumulator.Value.should_be(this.processor.Y.Value ^ byteToCompare);
-			};
-
-			it["should set negative flag equal to seventh accumulator bit"] = () =>
-			{
-				this.processor.Negative.should_be(this.processor.Accumulator.IsSignBitSet);
-			};
-
-			it["should set zero flag when accumulator is 0"] = () =>
-			{
-				this.processor.Zero.should_be(this.processor.Accumulator.IsValueEqualZero);
-			};
+			this.DefineSpecs(
+				(b) => accumulatorValue = b,
+				(b) => this.Memory.MockIndexedIndirectMemoryWrite(randomOffset, xRegisterValue, b));
 		}
 
 		public void When_use_indirect_indexed_addressing_mode()
 		{
+			var accumulatorValue = default(byte);
 			var randomOffset = default(byte);
-			var byteToCompare = default(byte);
 			var yRegisterValue = default(byte);
 
 			before = () =>
 			{
-				randomOffset = fixture.Create<byte>();
-				byteToCompare = fixture.Create<byte>();
-				yRegisterValue = fixture.Create<byte>();
-				this.memory.MockIndirectIndexedMemoryWrite(randomOffset, yRegisterValue, byteToCompare);
+				randomOffset = this.Fixture.Create<byte>();
+				yRegisterValue = this.Fixture.Create<byte>();
 			};
 
-			act = () =>
+			this.RunProgram(() => new byte[]
 			{
-				this.processor.RunProgram(new byte[]
+				(byte) OpCode.LDA_Immediate, accumulatorValue,
+				(byte) OpCode.LDY_Immediate, yRegisterValue,
+				(byte) OpCode.EOR_IndirectIndexed, randomOffset
+			});
+
+			this.DefineSpecs(
+				(b) => accumulatorValue = b,
+				(b) => this.Memory.MockIndirectIndexedMemoryWrite(randomOffset, yRegisterValue, b));
+		}
+
+		private void DefineSpecs(Action<byte> setAccumulatorValue, Action<byte> setByteToCompare)
+		{
+			before = () =>
+			{
+				setAccumulatorValue?.Invoke(0xa9);
+				setByteToCompare?.Invoke(0x05);
+			};
+
+			it["should set bitwise 'xor' result on accumulator"] = () => this.Processor.Accumulator.Value.should_be(0xac);
+
+			this.ZeroFlagShouldBeFalse();
+			this.NegativeFlagShouldBeTrue();
+
+			context["given that bitwise 'xor' has sign bit is set"] = () =>
+			{
+				before = () =>
 				{
-					(byte) OpCode.LDA_Absolute, fixture.Create<byte>(),
-					(byte) OpCode.TAX_Implied,
-					(byte) OpCode.LDY_Immediate, yRegisterValue,
-					(byte) OpCode.EOR_IndirectIndexed, randomOffset
-				});
+					setAccumulatorValue?.Invoke(0x80);
+					setByteToCompare?.Invoke(0x00);
+				};
+
+				this.NegativeFlagShouldBeTrue();
+				this.ZeroFlagShouldBeFalse();
 			};
 
-			it["should set bitwise exclusive 'or' result on accumulator"] = () =>
+			context["given that bitwise 'xor' has sign bit is 0x00"] = () =>
 			{
-				this.processor.Accumulator.Value.should_be(this.processor.X.ExlusiveOr(byteToCompare));
-			};
+				before = () =>
+				{
+					setAccumulatorValue?.Invoke(0x01);
+					setByteToCompare?.Invoke(0x01);
+				};
 
-			it["should set negative flag equal to seventh accumulator bit"] = () =>
-			{
-				this.processor.Negative.should_be(this.processor.Accumulator.IsSignBitSet);
-			};
-
-			it["should set zero flag when accumulator is 0"] = () =>
-			{
-				this.processor.Zero.should_be(this.processor.Accumulator.IsValueEqualZero);
+				this.ZeroFlagShouldBeTrue();
+				this.NegativeFlagShouldBeFalse();
 			};
 		}
 	}
