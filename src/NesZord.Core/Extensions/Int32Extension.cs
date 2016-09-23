7b8c1a59c2d61edc9e8cebfc69b1a65981432791
rmterra <r.mirandaterra@gmail.com>
2016-09-23 08:01:58 -0300

    Create extension methods to get page and offset from int

---
+++ src/NesZord.Core/Extensions/Int32Extension.cs
@@ -0,0 +1,15 @@
+﻿namespace NesZord.Core.Extensions
+{
+	public static class Int32Extension
+	{
+		public static byte GetPage(this int value)
+		{
+			return (byte)(value >> 8);
+		}
+
+		public static byte GetOffset(this int value)
+		{
+			return (byte)(value & 0xff);
+		}
+	}
+}
