71ef7d7ab3df5bccd5001bf0939bce2fb71f7ce9
rmterra <r.mirandaterra@gmail.com>
2018-06-08 00:41:56 -0300

    Create AccumulatorAddressingMode

---
+++ src/NesZord.Tests.New/AddressingMode/AccumulatorAddressingMode.cs
@@ -0,0 +1,19 @@
+﻿using NesZord.Core;
+using System;
+
+namespace NesZord.Tests.New.AddressingMode
+{
+	public class AccumulatorAddressingMode : IAddressingMode
+	{
+		private Microprocessor processor;
+
+		public byte OperationByte
+		{
+			get => this.processor.Accumulator.Value;
+			set => this.processor.Accumulator.Value = value;
+		}
+
+		public void Initialize(Microprocessor processor, MemoryMock memory)
+			=> this.processor = processor ?? throw new ArgumentNullException(nameof(processor));
+	}
+}
