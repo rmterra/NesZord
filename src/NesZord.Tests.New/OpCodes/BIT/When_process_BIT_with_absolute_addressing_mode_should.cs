2da392e124bef51fec64879711245aab19c62695
rmterra <r.mirandaterra@gmail.com>
2018-06-10 21:43:47 -0300

    migrate BIT specs to xUnit

---
+++ src/NesZord.Tests.New/OpCodes/BIT/When_process_BIT_with_absolute_addressing_mode_should.cs
@@ -0,0 +1,23 @@
+﻿using NesZord.Core;
+using NesZord.Tests.New.AddressingMode;
+
+namespace NesZord.Tests.New.OpCodes.BIT
+{
+	public class When_process_BIT_with_absolute_addressing_mode_should
+		: When_process_BIT_should<AbsoluteAddressingMode>
+	{
+		public When_process_BIT_with_absolute_addressing_mode_should() 
+			: base(new AbsoluteAddressingMode())
+		{
+		}
+
+		protected override void RunProgram()
+		{
+			this.Processor.RunProgram(new byte[]
+				{
+					(byte) OpCode.LDA_Immediate, this.Processor.Accumulator.Value,
+					(byte) OpCode.BIT_Absolute, this.AddressingMode.RandomOffset, this.AddressingMode.RandomPage
+				});
+		}
+	}
+}
---
+++ src/NesZord.Tests.New/OpCodes/BIT/When_process_BIT_with_zero_page_addressing_mode_should.cs
@@ -0,0 +1,23 @@
+﻿using NesZord.Core;
+using NesZord.Tests.New.AddressingMode;
+
+namespace NesZord.Tests.New.OpCodes.BIT
+{
+	public class When_process_BIT_with_zero_page_addressing_mode_should
+		: When_process_BIT_should<ZeroPageAddressingMode>
+	{
+		public When_process_BIT_with_zero_page_addressing_mode_should() 
+			: base(new ZeroPageAddressingMode())
+		{
+		}
+
+		protected override void RunProgram()
+		{
+			this.Processor.RunProgram(new byte[]
+				{
+					(byte) OpCode.LDA_Immediate, this.Processor.Accumulator.Value,
+					(byte) OpCode.BIT_ZeroPage, this.AddressingMode.RandomOffset
+				});
+		}
+	}
+}
