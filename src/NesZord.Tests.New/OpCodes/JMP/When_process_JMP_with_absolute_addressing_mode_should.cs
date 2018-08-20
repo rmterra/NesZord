c2067adb58a1959121b95261cc9c29d859d28bfb
rmterra <r.mirandaterra@gmail.com>
2018-08-20 00:26:23 -0300

    migrate JMP specs to xUnit

---
+++ src/NesZord.Tests.New/OpCodes/JMP/When_process_JMP_with_absolute_addressing_mode_should.cs
@@ -0,0 +1,14 @@
+﻿using NesZord.Core;
+using NesZord.Tests.New.AddressingMode;
+
+namespace NesZord.Tests.New.OpCodes.JMP
+{
+	public class When_process_JMP_with_absolute_addressing_mode_should
+		: When_process_JMP_should<AbsoluteAddressingMode>
+	{
+		public When_process_JMP_with_absolute_addressing_mode_should()
+			: base(new AbsoluteAddressingMode(OpCode.JMP_Absolute))
+		{
+		}
+	}
+}
---
+++ src/NesZord.Tests.New/OpCodes/JMP/When_process_JMP_with_indirect_addressing_mode_should.cs
@@ -0,0 +1,14 @@
+﻿using NesZord.Core;
+using NesZord.Tests.New.AddressingMode;
+
+namespace NesZord.Tests.New.OpCodes.JMP
+{
+	public class When_process_JMP_with_indirect_addressing_mode_should
+		: When_process_JMP_should<IndirectIndexedAddressingMode>
+	{
+		public When_process_JMP_with_indirect_addressing_mode_should()
+			: base(new IndirectIndexedAddressingMode(OpCode.JMP_Indirect))
+		{
+		}
+	}
+}
