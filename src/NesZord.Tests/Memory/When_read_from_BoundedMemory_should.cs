1e82d2501a45a8ed0a6622f2e046512e701c7d6f
rmterra <r.mirandaterra@gmail.com>
2018-11-09 00:40:19 -0200

    Add tests for BoundedMemory

---
+++ src/NesZord.Tests/Memory/When_read_from_BoundedMemory_should.cs
@@ -0,0 +1,70 @@
+﻿using FluentAssertions;
+using NesZord.Core.Memory;
+using System;
+using Xunit;
+
+namespace NesZord.Tests.Memory
+{
+	public class When_read_from_BoundedMemory_should
+	{
+		private BoundedMemory memory;
+
+		public When_read_from_BoundedMemory_should()
+		{
+			var firstAddress = new MemoryLocation(0x10, 0x00);
+			var lastAddress = new MemoryLocation(0x20, 0x00);
+
+			this.memory = new BoundedMemory(firstAddress, lastAddress);
+		}
+
+		[Fact]
+		public void Throw_ArgumentRangeException_when_received_location_is_below_of_FirstAddress()
+		{
+			// Act
+			var location = new MemoryLocation(0x05, 0x00);
+			Action act = () => this.memory.Read(location);
+
+			// Assert
+			act.Should().Throw<ArgumentOutOfRangeException>();
+		}
+
+		[Fact]
+		public void Throw_ArgumentRangeException_when_received_location_is_above_of_LastAddress()
+		{
+			// Act
+			var location = new MemoryLocation(0xff, 0x00);
+			Action act = () => this.memory.Read(location);
+
+			// Assert
+			act.Should().Throw<ArgumentOutOfRangeException>();
+		}
+
+		[Fact]
+		public void Read_value_from_received_location_when_value_is_above_of_FirstAddress()
+		{
+			// Arrange
+			var location = new MemoryLocation(0x11, 0x00);
+			this.memory.Write(location, 1);
+
+			// Act
+			var received = this.memory.Read(location);
+
+			// Assert
+			received.Should().Be(1);
+		}
+
+		[Fact]
+		public void Read_value_from_received_location_when_value_is_below_of_LastAddress()
+		{
+			// Arrange
+			var location = new MemoryLocation(0x1f, 0x00);
+			this.memory.Write(location, 1);
+
+			// Act
+			var received = this.memory.Read(location);
+
+			// Assert
+			received.Should().Be(1);
+		}
+	}
+}
---
+++ src/NesZord.Tests/Memory/When_write_on_BondedMemory_should.cs
@@ -0,0 +1,68 @@
+﻿using FluentAssertions;
+using NesZord.Core.Memory;
+using System;
+using Xunit;
+
+namespace NesZord.Tests.Memory
+{
+	public class When_write_on_BondedMemory_should
+	{
+		private BoundedMemory memory;
+
+		public When_write_on_BondedMemory_should()
+		{
+			var firstAddress = new MemoryLocation(0x10, 0x00);
+			var lastAddress = new MemoryLocation(0x20, 0x00);
+
+			this.memory = new BoundedMemory(firstAddress, lastAddress);
+		}
+
+		[Fact]
+		public void Throw_ArgumentRangeException_when_received_location_is_below_of_FirstAddress()
+		{
+			// Act
+			var location = new MemoryLocation(0x05, 0x00);
+			Action act = () => this.memory.Write(location, 1);
+
+			// Assert
+			act.Should().Throw<ArgumentOutOfRangeException>();
+		}
+
+		[Fact]
+		public void Throw_ArgumentRangeException_when_received_location_is_above_of_LastAddress()
+		{
+			// Act
+			var location = new MemoryLocation(0xff, 0x00);
+			Action act = () => this.memory.Write(location, 1);
+
+			// Assert
+			act.Should().Throw<ArgumentOutOfRangeException>();
+		}
+
+		[Fact]
+		public void Store_received_value_when_value_is_above_of_FirstAddress()
+		{
+			// Arrange
+			var location = new MemoryLocation(0x11, 0x00);
+
+			// Act
+			this.memory.Write(location, 1);
+
+			// Assert
+			this.memory.Read(location).Should().Be(1);
+		}
+
+		[Fact]
+		public void Store_received_value_when_value_is_below_of_LastAddress()
+		{
+			// Arrange
+			var location = new MemoryLocation(0x1f, 0x00);
+
+			// Act
+			this.memory.Write(location, 1);
+
+			// Assert
+			this.memory.Read(location).Should().Be(1);
+		}
+	}
+}
