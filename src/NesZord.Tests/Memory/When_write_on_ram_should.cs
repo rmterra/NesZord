6bb3ffd9b30332e5628210fae642073830d765d3
rmterra <r.mirandaterra@gmail.com>
2018-11-10 00:36:23 -0200

    Add tests for Ram

---
+++ src/NesZord.Tests/Memory/When_write_on_ram_should.cs
@@ -0,0 +1,206 @@
+﻿using FluentAssertions;
+using NesZord.Core.Memory;
+using Xunit;
+
+namespace NesZord.Tests.Memory
+{
+	public class When_write_on_ram_should
+	{
+		private Ram ram;
+
+		public When_write_on_ram_should()
+		{
+			this.ram = new Ram();
+		}
+
+		[Fact]
+		public void Given_that_received_a_main_address_should_be_possible_to_read_stored_value()
+		{
+			// Arrange
+			var address = new MemoryAddress(0x00, 0x01);
+
+			// Act -> Assert
+			this.Given_that_received_an_address_should_be_possible_to_read_stored_value(address, 0x05);
+		}
+
+		[Fact]
+		public void Given_that_received_a_main_address_should_replicate_stored_value_to_mirror1()
+		{
+			// Arrange
+			var address = new MemoryAddress(0x00, 0x01);
+			var mirrorAddress = new MemoryAddress(0x08, 0x01);
+
+			// Act -> Assert
+			this.Given_that_received_an_address_should_replicate_stored_value_to_another_address(address, mirrorAddress, 0x05);
+		}
+
+		[Fact]
+		public void Given_that_received_a_main_address_should_replicate_stored_value_to_mirror2()
+		{
+			// Arrange
+			var address = new MemoryAddress(0x00, 0x01);
+			var mirrorAddress = new MemoryAddress(0x10, 0x01);
+
+			// Act -> Assert
+			this.Given_that_received_an_address_should_replicate_stored_value_to_another_address(address, mirrorAddress, 0x05);
+		}
+
+		[Fact]
+		public void Given_that_received_a_main_address_should_replicate_stored_value_to_mirror3()
+		{
+			// Arrange
+			var address = new MemoryAddress(0x00, 0x01);
+			var mirrorAddress = new MemoryAddress(0x18, 0x01);
+
+			// Act -> Assert
+			this.Given_that_received_an_address_should_replicate_stored_value_to_another_address(address, mirrorAddress, 0x05);
+		}
+
+		[Fact]
+		public void Given_that_received_a_mirror1_address_should_be_possible_to_read_stored_value()
+		{
+			// Arrange
+			var address = new MemoryAddress(0x08, 0x01);
+
+			// Act -> Assert
+			this.Given_that_received_an_address_should_be_possible_to_read_stored_value(address, 0x05);
+		}
+
+		[Fact]
+		public void Given_that_received_a_mirror1_address_should_replicate_stored_value_to_main_memory()
+		{
+			// Arrange
+			var address = new MemoryAddress(0x08, 0x01);
+			var mirrorAddress = new MemoryAddress(0x00, 0x01);
+
+			// Act -> Assert
+			this.Given_that_received_an_address_should_replicate_stored_value_to_another_address(address, mirrorAddress, 0x05);
+		}
+
+		[Fact]
+		public void Given_that_received_a_mirror1_address_should_replicate_stored_value_to_mirror2()
+		{
+			// Arrange
+			var address = new MemoryAddress(0x08, 0x01);
+			var mirrorAddress = new MemoryAddress(0x10, 0x01);
+
+			// Act -> Assert
+			this.Given_that_received_an_address_should_replicate_stored_value_to_another_address(address, mirrorAddress, 0x05);
+		}
+
+		[Fact]
+		public void Given_that_received_a_mirror1_address_should_replicate_stored_value_to_mirror3()
+		{
+			// Arrange
+			var address = new MemoryAddress(0x08, 0x01);
+			var mirrorAddress = new MemoryAddress(0x18, 0x01);
+
+			// Act -> Assert
+			this.Given_that_received_an_address_should_replicate_stored_value_to_another_address(address, mirrorAddress, 0x05);
+		}
+
+		[Fact]
+		public void Given_that_received_a_mirror2_address_should_be_possible_to_read_stored_value()
+		{
+			// Arrange
+			var address = new MemoryAddress(0x10, 0x01);
+
+			// Act -> Assert
+			this.Given_that_received_an_address_should_be_possible_to_read_stored_value(address, 0x05);
+		}
+
+		[Fact]
+		public void Given_that_received_a_mirror2_address_should_replicate_stored_value_to_main_memory()
+		{
+			// Arrange
+			var address = new MemoryAddress(0x10, 0x01);
+			var mirrorAddress = new MemoryAddress(0x00, 0x01);
+
+			// Act -> Assert
+			this.Given_that_received_an_address_should_replicate_stored_value_to_another_address(address, mirrorAddress, 0x05);
+		}
+
+		[Fact]
+		public void Given_that_received_a_mirror2_address_should_replicate_stored_value_to_mirror1()
+		{
+			// Arrange
+			var address = new MemoryAddress(0x10, 0x01);
+			var mirrorAddress = new MemoryAddress(0x08, 0x01);
+
+			// Act -> Assert
+			this.Given_that_received_an_address_should_replicate_stored_value_to_another_address(address, mirrorAddress, 0x05);
+		}
+
+		[Fact]
+		public void Given_that_received_a_mirror2_address_should_replicate_stored_value_to_mirror3()
+		{
+			// Arrange
+			var address = new MemoryAddress(0x10, 0x01);
+			var mirrorAddress = new MemoryAddress(0x18, 0x01);
+
+			// Act -> Assert
+			this.Given_that_received_an_address_should_replicate_stored_value_to_another_address(address, mirrorAddress, 0x05);
+		}
+
+		[Fact]
+		public void Given_that_received_a_mirror3_address_should_be_possible_to_read_stored_value()
+		{
+			// Arrange
+			var address = new MemoryAddress(0x18, 0x01);
+
+			// Act -> Assert
+			this.Given_that_received_an_address_should_be_possible_to_read_stored_value(address, 0x05);
+		}
+
+		[Fact]
+		public void Given_that_received_a_mirror3_address_should_replicate_stored_value_to_main_memory()
+		{
+			// Arrange
+			var address = new MemoryAddress(0x18, 0x01);
+			var mirrorAddress = new MemoryAddress(0x00, 0x01);
+
+			// Act -> Assert
+			this.Given_that_received_an_address_should_replicate_stored_value_to_another_address(address, mirrorAddress, 0x05);
+		}
+
+		[Fact]
+		public void Given_that_received_a_mirror3_address_should_replicate_stored_value_to_mirror1()
+		{
+			// Arrange
+			var address = new MemoryAddress(0x18, 0x01);
+			var mirrorAddress = new MemoryAddress(0x08, 0x01);
+
+			// Act -> Assert
+			this.Given_that_received_an_address_should_replicate_stored_value_to_another_address(address, mirrorAddress, 0x05);
+		}
+
+		[Fact]
+		public void Given_that_received_a_mirror3_address_should_replicate_stored_value_to_mirror2()
+		{
+			// Arrange
+			var address = new MemoryAddress(0x18, 0x01);
+			var mirrorAddress = new MemoryAddress(0x10, 0x01);
+
+			// Act -> Assert
+			this.Given_that_received_an_address_should_replicate_stored_value_to_another_address(address, mirrorAddress, 0x05);
+		}
+
+		private void Given_that_received_an_address_should_be_possible_to_read_stored_value(MemoryAddress address, byte value)
+		{
+			// Act
+			this.ram.Write(address, value);
+
+			// Assert
+			this.ram.Read(address).Should().Be(value);
+		}
+
+		private void Given_that_received_an_address_should_replicate_stored_value_to_another_address(MemoryAddress address, MemoryAddress another, byte value)
+		{
+			// Act
+			this.ram.Write(address, value);
+
+			// Assert
+			this.ram.Read(another).Should().Be(value);
+		}
+	}
+}
